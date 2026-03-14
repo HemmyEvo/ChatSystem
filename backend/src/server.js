4f80f8dae0c1e8efe463c58eb9a5630195b7650f
HemmyEvo <xxxlildevil999@gmail.com>
2026-03-14 13:56:39 +0100

    update server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,36 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import authRoutes from './controller/routes/auth.js';
 import messageRoutes from './controller/routes/message.js';
+import path from 'path';
+
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
+
 const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
 
-// Your API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/auth", authRoutes);
+app.use("/message", messageRoutes);
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+
+
+console.log(`Environment: ${process.env.NODE_ENV}`);
+//make ready for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (_, res) => { 
+    res.sendFile(path.join(__dirname, '../frontend','dist', 'index.html'));
   });
 }
 
-// CRITICAL FOR VERCEL: Export the app as a module
-export default app;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
