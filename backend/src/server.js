35967fd81203fe7278ddd35f4544d630d5236588
HemmyEvo <xxxlildevil999@gmail.com>
2026-03-14 14:14:46 +0100

    update thing to deploy to vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,36 +1,25 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import authRoutes from './controller/routes/auth.js';
 import messageRoutes from './controller/routes/message.js';
-import path from 'path';
-
 
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
-
-
 const port = process.env.PORT || 3000;
-
 
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/message", messageRoutes);
-
-
+// 1. ADDED "/api" prefix so it matches your vercel.json rewrites
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 console.log(`Environment: ${process.env.NODE_ENV}`);
-//make ready for deployment
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (_, res) => { 
-    res.sendFile(path.join(__dirname, '../frontend','dist', 'index.html'));
+if (!process.env.VERCEL) {
+  app.listen(port, () => {
+    console.log(`Server is running locally on http://localhost:${port}`);
   });
 }
 
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
+export default app;
