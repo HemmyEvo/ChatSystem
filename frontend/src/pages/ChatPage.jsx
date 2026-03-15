e27083ccd3cc196a2fad0ce3f3a1d0d0bb6fd508
HemmyEvo <xxxlildevil999@gmail.com>
2026-03-16 00:00:05 +0100

    update logout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,7 +9,7 @@ function ChatPage() {
   return (
     <div>
 
-    <button onClick={handleLogout} className='bg-blue-500 text-white px-4 py-2 rounded'>
+    <button onClick={() =>handleLogout()} className='bg-blue-500 text-white px-4 py-2 rounded'>
       Log out
     </button>
 
